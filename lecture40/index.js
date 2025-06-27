dd9268c965bbfac5fd6bcc76a3094f6d93082295
lavinachauhan <chauhanlavi1627@gmail.com>
2025-06-27 19:46:03 +0530

    javascript learning start

---
+++ lecture40/index.js
@@ -0,0 +1,26 @@
+var age = 20;
+if(true){
+    console.log(age);
+} 
+var num = 10;
+console.log(num);
+num = 30;
+console.log(num);
+
+let myVar = "lavina"
+console.log(myVar);
+
+// myVar = 25;
+// console.log(myVar);
+
+const num2 = 20;
+console.log(num2);
+// num2 = 40;
+// console.log(num2);
+
+let num3 = null;
+console.log(num3);
+console.log(typeof(num3));
+console.log(typeof(num2));
+console.log(typeof(myVar));
+
